3d47c69c8010c91d88c5532f79bdd5d12eb702d3
Sharainwy <saksit.lit@gmail.com>
2023-08-16 14:46:30 +0700

    443

--- index.js
+++ index.js
@@ -413,6 +413,6 @@ app.listen(port, () => {
   console.log(`listening on ${port}`);
 });
 
-server.listen(3001, () => {
-  console.log('Server is listening on port 3001');
+server.listen(443, () => {
+  console.log('Server is listening on port 443');
 });
